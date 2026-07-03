e0826fbb729d2ea7316cd6b26ebc67753d06b0d6
dev <dev@local>
2026-07-03 16:37:23 +0000

    fix: download shaderc source at configure time via FetchContent
    ExternalProject_Add defers download to build time, so headers weren't
    available when main project compiled. Use FetchContent_Populate to download
    shaderc source at configure time, then build it as ExternalProject with
    DOWNLOAD_COMMAND empty (source already available).

--- yabause/src/vulkan/external_shaderc.cmake
+++ yabause/src/vulkan/external_shaderc.cmake
@@ -1,12 +1,39 @@
+include(FetchContent)
 include(ExternalProject)
+
+# Download shaderc source at configure time so headers are available
+FetchContent_Declare(
+    shaderc_src
+    GIT_REPOSITORY https://github.com/google/shaderc.git
+    GIT_TAG v2020.5
+    GIT_SHALLOW TRUE
+)
+FetchContent_Populate(shaderc_src)
+set(SHADERC_SOURCE_DIR ${shaderc_src_SOURCE_DIR})
+
+# Run git-sync-deps to get glslang, spirv-tools, etc.
+execute_process(
+    COMMAND python3 utils/git-sync-deps
+    WORKING_DIRECTORY ${SHADERC_SOURCE_DIR}
+    RESULT_VARIABLE GIT_SYNC_RESULT
+)
+if(NOT GIT_SYNC_RESULT EQUAL 0)
+    message(WARNING "git-sync-deps failed, trying python...")
+    execute_process(
+        COMMAND python utils/git-sync-deps
+        WORKING_DIRECTORY ${SHADERC_SOURCE_DIR}
+    )
+endif()
+
+set(SHADERC_INCLUDE_DIR ${SHADERC_SOURCE_DIR}/libshaderc/include)
 
 if(ANDROID)
 get_filename_component(TOOL_CHAIN_ABSOLUTE_PATH "${CMAKE_TOOLCHAIN_FILE}"
                        REALPATH BASE_DIR "${CMAKE_BINARY_DIR}")
 
-set( ADDITIONAL_CMAKE_ARGS 
+set( ADDITIONAL_CMAKE_ARGS
  -DANDROID_ABI=${ANDROID_ABI}
- -DCMAKE_MAKE_PROGRAM=${CMAKE_MAKE_PROGRAM} 
+ -DCMAKE_MAKE_PROGRAM=${CMAKE_MAKE_PROGRAM}
  -DANDROID_NATIVE_API_LEVEL=${ANDROID_NATIVE_API_LEVEL}
  -DCMAKE_TOOLCHAIN_FILE=${TOOL_CHAIN_ABSOLUTE_PATH}
 )
@@ -14,12 +41,13 @@ else()
   set(ADDITIONAL_CMAKE_ARGS "")
 endif()
 
-
+# Build shaderc as external project (using already-downloaded source)
 ExternalProject_Add(
-    shaderc 
+    shaderc
     PREFIX shaderc
-    GIT_REPOSITORY https://github.com/google/shaderc.git
-    GIT_TAG v2020.5
+    SOURCE_DIR ${SHADERC_SOURCE_DIR}
+    DOWNLOAD_COMMAND ""
+    UPDATE_COMMAND ""
     CMAKE_ARGS
         -DSHADERC_SKIP_INSTALL=OFF
         -DSHADERC_SKIP_TESTS=ON
@@ -30,28 +58,12 @@ ExternalProject_Add(
         ${ADDITIONAL_CMAKE_ARGS}
 )
 
-ExternalProject_Get_Property(shaderc SOURCE_DIR)
 ExternalProject_Get_Property(shaderc BINARY_DIR)
-
-ExternalProject_Add_Step(
-    shaderc
-    copySource
-    DEPENDEES download
-    DEPENDERS configure
-    WORKING_DIRECTORY ${SOURCE_DIR}
-    COMMAND python3 utils/git-sync-deps
-    COMMENT "utils/git-sync-deps"
-)
-
-
 ExternalProject_Get_Property(shaderc INSTALL_DIR)
 
-set(SHADERC_INCLUDE_DIR ${SOURCE_DIR}/libshaderc/include)
 set(SHADERC_LIBRARY_DIR ${BINARY_DIR}/libshaderc )
 if(MSVC)
   set(SHADERC_LIBRARIES ${INSTALL_DIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}shaderc_combined${CMAKE_STATIC_LIBRARY_SUFFIX}  )
 else()
-  set(SHADERC_LIBRARIES -lshaderc_combined )
+  set(SHADERC_LIBRARIES ${BINARY_DIR}/libshaderc/${CMAKE_STATIC_LIBRARY_PREFIX}shaderc_combined${CMAKE_STATIC_LIBRARY_SUFFIX} )
 endif()
-
-
